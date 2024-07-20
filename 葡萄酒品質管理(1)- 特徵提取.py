4ca6fbe209003b68ecf46b4e4183dfcd9f0952e6
Jimmy-Lin-yu <54769719+Jimmy-Lin-yu@users.noreply.github.com>
2024-07-20 13:54:29 +0800

    Update 葡萄酒品質管理(1)- 特徵提取.py

--- 葡萄酒品質管理(1)- 特徵提取.py
+++ 葡萄酒品質管理(1)- 特徵提取.py
@@ -1,7 +1,7 @@
 import numpy as np                                                             
 import pandas as pd 
  
-df_wine = pd.read_csv('https://archive.ics.uci.edu/' 
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'  
                       'ml/machine-learning-databases/wine/wine.data',
                       header=None)
 
